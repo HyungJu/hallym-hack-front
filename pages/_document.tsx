fb7038ea7ceb9aa99b1783830f9ed91e04b3b873
HyungJu Sung <sungkisa@naver.com>
2022-10-31 15:53:58 +0900

    ff

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
